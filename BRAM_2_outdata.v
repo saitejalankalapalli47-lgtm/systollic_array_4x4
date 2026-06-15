bf941e58ed4239eb81135b020242c0eee2daaed5
Saiteja1909 <saitejalankalapalli47@gmail.com>
2026-06-15 15:27:40 +0530

    Create BRAM_2_outdata.v

---
+++ BRAM_2_outdata.v
@@ -0,0 +1,44 @@
+
+
+
+module BRAM_2_outdata #(
+    parameter DATA_WIDTH = 16,
+    parameter BUFFER_DEPTH = 32,
+    parameter ADDR_WIDTH = $clog2(BUFFER_DEPTH)
+)(
+    input wire clk,
+    input wire rst_n,
+    input wire wr_en,
+    input wire re_en,
+    input wire [ADDR_WIDTH-1:0] wr_addr,
+    input wire [ADDR_WIDTH-1:0] rd_addr1,
+    input wire [ADDR_WIDTH-1:0] rd_addr2,
+    input wire signed [DATA_WIDTH-1:0] data_in,
+    output reg signed [DATA_WIDTH-1:0] data_out1,
+    output reg signed [DATA_WIDTH-1:0] data_out2
+);
+
+ 
+    (* ram_style = "block" *) 
+    reg signed [DATA_WIDTH-1:0] ram [BUFFER_DEPTH-1:0];
+
+    always @(posedge clk) begin
+    if (!rst_n) begin
+            data_out1 <= 0;
+            data_out2 <= 0;
+    end
+    else begin
+      // Write Operation
+        if (wr_en) begin
+            ram[wr_addr] <= data_in;
+        end
+
+    // Read Operation (Synchronous BRAM)
+        if (re_en) begin
+            data_out1 <= ram[rd_addr1];
+            data_out2 <= ram[rd_addr2];
+        end
+    end
+    end
+
+endmodule
